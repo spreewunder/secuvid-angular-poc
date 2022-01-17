70cf5b7c2e4e21c048f6d4bdf5e4407155d97e79
oliver.jungeilges <oliver.jungeilges@spreewunder.com>
2022-01-17 16:25:28 +0100

    Made progress in regards to custom webpack conf

--- extra-webpack-config.js
+++ extra-webpack-config.js
@@ -1,17 +1,10 @@
 const webpack = require('webpack');
 const TerserPlugin = require("terser-webpack-plugin");
 
+
 console.log("Using custom partial webpack config");
 
-module.exports = {
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin({
-      terserOptions: {
-        mangle: false,
-        keep_classnames: true,
-        compress: false,
-      }
-    })]
-  }
-}
+module.exports = (config, options, targetOptions) => {
+    //config.optimization.minimizer.minimize = true;
+    return config;
+};
